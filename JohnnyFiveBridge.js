cd642b27ca4110b9defb6414ea817cc6f1bb46fd
David Janes <davidjanes@davidjanes.com>
2016-02-06 10:27:06 -0500

    fixed up 'push' validation

--- JohnnyFiveBridge.js
+++ JohnnyFiveBridge.js
@@ -230,7 +230,7 @@ JohnnyFiveBridge.prototype.push = function (pushd, done) {
         return;
     }
 
-    self._validate_push(pushd);
+    self._validate_push(pushd, done);
 
     logger.info({
         method: "push",
